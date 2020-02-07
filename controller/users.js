ee779bdca541dece4ccf95296d2cdc5ea312ffcd
yjanah-ext@integra.fr <yjanah-ext@integra.fr>
2020-02-07 19:59:23 +0100

    refactoring

--- controller/users.js
+++ controller/users.js
@@ -10,7 +10,34 @@ let users = JSON.parse(Buffer.concat([personsChunk]).toString());
 
 // Retourner les users depuis le fichier users.json
 const getUsers = (req, res, next) => {
-    res.send(JSON.stringify(users));
+    let tbody = '';
+    users.map( user => {
+        tbody += `
+                <tr>
+                    <td>${user.name}</td>
+                    <td>${user.age}</td>
+                    <td>${user.profession}</td>
+                </tr>`
+    })
+    res.send(`
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        </head>
+        <body>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Age</th>
+                        <th>Profession</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tbody}
+                </tbody>
+            </table>
+        </body>
+    `);
 };
 
 // Sauvegarder un utilisateur
@@ -22,10 +49,11 @@ const saveUser = (req, res, next) => {
 
 
 // Supprimer un utilisateur
-const saveUser = (req, res, next) => {
+const deleteUser = (req, res, next) => {
     console.log(req.body);
-    fs.writeFileSync(_source, JSON.stringify(users.concat(req.body)));
-    res.redirect('/users');
+
+    // fs.writeFileSync(_source, JSON.stringify(users.filter(it => it.id)));
+    // res.redirect('/users');
 };
 
 // Retourner le formulaire de création d'un user
@@ -50,5 +78,6 @@ const getForm = (req, res, next) => {
 module.exports = {
     getForm: (PATH, APP) => APP.get(PATH, getForm),
     getUsers: (PATH, APP) => APP.get(PATH, getUsers),
-    saveUser: (PATH, APP) => APP.post(PATH, saveUser)
+    saveUser: (PATH, APP) => APP.post(PATH, saveUser),
+    deleteUser: (PATH, APP) => APP.delete(PATH, deleteUser)
 };
